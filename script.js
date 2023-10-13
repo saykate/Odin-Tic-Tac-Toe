5a29ca6a9a822b2ee035fd13b1df981874239dc8
Katie Sayers <kate@student.dev-island.org>
2023-10-12 16:37:48 -1000

    Added a modal for the players to add their names,Switched the alert at winning to a modal.

--- script.js
+++ script.js
@@ -1,8 +1,58 @@
 //why would the Players need an object?
-//create Players per the project instructions
+//add a check for the Tie
+//figure out win glitch
+let playerX = '';
+let playerO = '';
+const overlay = document.querySelector('.overlay');
+
+const playerInfo = (() => {
+    const xModal = document.querySelector('.x-modal');
+    const oModal = document.querySelector('.o-modal');
+    const startButton = document.querySelector('.start-button');
+    const submits = document.querySelectorAll('.submit');
+
+    const openModal = function (event) {
+            xModal.classList.remove('hidden');
+            overlay.classList.remove('hidden');
+        }
+
+    const closeModal = function (event) {
+        if(event.target.classList.contains('x-submit')) {
+            const xInput = document.getElementById('x-name-modal');
+            playerX = xInput.value;
+
+            if(playerX.trim() === '') {
+                return alert('Please enter a name for player X')
+            }
+
+            xModal.classList.add('hidden');
+            oModal.classList.remove('hidden');
+
+        } else if(event.target.classList.contains('o-submit')) {
+            const oInput = document.getElementById('o-name-modal');
+            playerO = oInput.value;
+
+            if(playerO.trim() === '') {
+                return alert('Please enter a name for player O')
+            }
+
+            oModal.classList.add('hidden');
+            overlay.classList.add('hidden');
+            gameBoard();
+        }
+    }
+
+    startButton.addEventListener('click', openModal);
+    submits.forEach(submit => {
+        submit.addEventListener('click', closeModal);
+    })
+})();
 
 const gameBoard = (() => {
     const gameCell = document.querySelectorAll('.game-cell');
+    const winModal = document.querySelector('.win-modal');
+    const winnerText = document.querySelector('.winner-text');
+    const restart = document.querySelector('.restart-button');
 
     const gameArray = [
         [null, null, null], 
@@ -40,7 +90,9 @@ const gameBoard = (() => {
             (gameArray[0][2] === currentPlayer && gameArray[1][2] === currentPlayer && gameArray[2][2] === currentPlayer) || 
             (gameArray[0][0] === currentPlayer && gameArray[1][1] === currentPlayer && gameArray[2][2] === currentPlayer) || 
             (gameArray[0][2] === currentPlayer && gameArray[1][1] === currentPlayer && gameArray[2][0] === currentPlayer)) {
-                alert(`${currentPlayer}'s for the WIN!!!`);
+                winnerText.textContent = `${currentPlayer === 'X' ? playerX : playerO} is the WINNER!!!`;
+                winModal.classList.remove('hidden');
+                overlay.classList.remove('hidden');
             } else {
             return
         }
@@ -65,11 +117,17 @@ const gameBoard = (() => {
 
     };
 
+    const restartGame = function() {
+        winModal.classList.add('hidden');
+        overlay.classList.add('hidden');
+    }
+
        //listen to clicks on each game cell
     gameCell.forEach(cell => {
          cell.addEventListener('click', playGame);
     })
+    restart.addEventListener('click', restartGame);
 
-})();
+});
 
   
